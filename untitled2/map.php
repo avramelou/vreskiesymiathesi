02074543681f9f3d645806c0e26831e8e3a73798
avramelou <avramelou@csd.auth.gr>
2020-06-01 16:46:04 +0300

    map php

--- untitled2/map.php
+++ untitled2/map.php
@@ -36,24 +36,56 @@
     </div>
     <a href="login.html"><button type="button" class="btn navbar-button"><i class="fas fa-user"></i> Σύνδεση</button></a>
 </nav>
+<?php
+$searh="";
+if ($_POST['submit']=="submit") {
+    $searh=$_POST['search'];
+}
+?>
+
 
 <div class="map">
     <div class="map-text">
         <h5>Εύρεση θέσης πάρκινγκ</h5><br>
         <p>Αναζητήστε μία θέση πάρκινγκ στο Δήμο της Θεσσαλλονίκης.<br>Πατήστε μία θέση στον χάρτη για να δείτε που βρίσκεται ή αναζητήστε μια θέση πάρκινγκ κοντά σας.</p>
         <div class="search-box-wrapper">
-            <input type="text" id="search" placeholder="Αναζήτηση..." class="search-box-input">
-            <button class="search-box-button" type="submit"><i class="fa fa-search"></i></button>
+            <form method="post" action="map.php">
+                <input type="text" id="search" placeholder="Αναζήτηση..." name="search" class="search-box-input"  value="<?=$searh;?>">
+                <button class="search-box-button" type="submit" name="submit" value="submit"><i class="fa fa-search"></i></button>
+            </form>
         </div>
+        <?php
+        if(!empty($searh))
+        {
+            $link=mysqli_connect('localhost',"avramelou","eresos4ever","mapdb");
+            $sql="SELECT * FROM PARKING WHERE ΟΔΟΣ='$searh' OR ΠΕΡΙΟΧΗ='$searh'";
+            $result=mysqli_query($link,$sql);
+            if(mysqli_num_rows($result)==0)
+            {
+                echo "<p><br><br> ΔΕΝ ΒΡΕΘΗΚΑΝ ΘΕΣΕΙΣ ΠΑΡΚΙΝΓΚ. </p>";
+            }
+            for ($i=0; $i<mysqli_num_rows($result); $i++) {
+                $row = mysqli_fetch_assoc($result);
+                $show=$row["ΟΔΟΣ"] .' ' .$row["ΑΡΙΘΜΟΣ"] .' ' . $row["ΠΕΡΙΟΧΗ"];
+                $site=$row["SITE"];
+                echo "<p> <br><br> $show <a href='$site' target='_blank' title='Άνοιγμα στο GoogleMaps'> <i class=\"fa fa-location-arrow\" aria-hidden=\"true\"></i></a> </p>";
+            }
+        }
+        ?>
+
+
+
     </div>
+
     <div >
         <iframe class="map-frame" src="https://www.google.com/maps/d/embed?mid=1FJTcgJvioT3JF0h0GYj7WNzUtmOH-efl"></iframe>
     </div>
 </div>
 
 
+
 <footer class="footer">
-    <div class="fb-share-button" data-href="http://localhost:63342/vreskiesymiathsi/untitled2/index.html?_ijt=drbuie6ggtrueacrt4gcv9a29l" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A63342%2Fvreskiesymiathsi%2Funtitled2%2Findex.html%3F_ijt%3Ddrbuie6ggtrueacrt4gcv9a29l&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+    <div class="fb-share-button" data-href="http://83.212.77.226/index.html" data-layout="button" data-size="small"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A63342%2Fvreskiesymiathsi%2Funtitled2%2Findex.html%3F_ijt%3Ddrbuie6ggtrueacrt4gcv9a29l&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
 </footer>
 
